0d5c848618659712a978380af26567c7dd19ca72
devika <devikas5331@gmail.com>
2021-12-06 14:20:52 +0530

    new commit

--- src/model/bookdata.js
+++ src/model/bookdata.js
@@ -1,9 +1,10 @@
 //ACCESSING MONGOOSE PACKAGE
 const mongoose = require('mongoose');
 // DATABASE CONNECTION
-//mongoose.connect('mongodb+srv://userone:userone@ictakfsd.hnync.mongodb.net/LIBRARYAPP?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://libraryappnew:libraryappnew@ictakfsd.hnync.mongodb.net/LibraryAppNew?retryWrites=true&w=majority');
 
- mongoose.connect('mongodb://localhost:27017/librarynew');
+
+ //mongoose.connect('mongodb://localhost:27017/librarynew');
 
 //SCHEMA DEFINITION
 const Schema = mongoose.Schema;
